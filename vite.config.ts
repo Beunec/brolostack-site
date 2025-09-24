fc877cbd3b97523f51bcddf79f002aa532a6be4f
Olu Akinnawo <oma57@rutgers.edu>
2025-09-25 04:53:46 +1000

    Production-ready Vercel deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   define: {
     global: 'globalThis',
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
     'process.env': JSON.stringify(process.env || {}),
   },
   optimizeDeps: {
