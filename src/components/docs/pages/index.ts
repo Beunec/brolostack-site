78c34a614a0128da17ad40bb350026fac6104534
Olu Akinnawo <oma57@rutgers.edu>
2025-09-25 05:02:31 +1000

    Fix build error: Create pages index.ts for proper exports

---
+++ src/components/docs/pages/index.ts
@@ -0,0 +1,63 @@
+// Export all page components
+export { default as NotFoundPage } from './NotFoundPage';
+
+// Export AI Framework pages
+export { default as AIFrameworkPage } from './ai-framework/AIFrameworkPage';
+export { default as AIFrameworkGuidePage } from './ai-framework/AIFrameworkGuidePage';
+
+// Export Authentication pages
+export { default as AuthenticationGuidePage } from './authentication/AuthenticationGuidePage';
+export { default as CIAMIntegrationPage } from './authentication/CIAMIntegrationPage';
+
+// Export Backend Integration pages
+export { default as BackendIntegrationPage } from './backend-integration/BackendIntegrationPage';
+export { default as BackendIntegrationGuidePage } from './backend-integration/BackendIntegrationGuidePage';
+
+// Export Brolostack Worker pages
+export { default as BrolostackWorkerPage } from './brolostack-worker/BrolostackWorkerPage';
+export { default as BrolostackWorkerGuidePage } from './brolostack-worker/BrolostackWorkerGuidePage';
+
+// Export Browser Compatibility pages
+export { default as BrowserCompatibilityPage } from './browser-compatibility/BrowserCompatibilityPage';
+export { default as BrowserCompatibilityGuidePage } from './browser-compatibility/BrowserCompatibilityGuidePage';
+export { default as BrowserPrivateModePage } from './browser-compatibility/BrowserPrivateModePage';
+
+// Export Cloud Integration pages
+export { default as CloudIntegrationPage } from './cloud-integration/CloudIntegrationPage';
+
+// Export Community pages
+export { default as CommunityGuidePage } from './community/CommunityGuidePage';
+
+// Export Enterprise Features pages
+export { default as EnterpriseFeaturesPage } from './enterprise-features/EnterpriseFeaturesPage';
+export { default as EnterpriseFeaturesGuidePage } from './enterprise-features/EnterpriseFeaturesGuidePage';
+
+// Export Environment Management pages
+export { default as EnvironmentManagementPage } from './environment-management/EnvironmentManagementPage';
+export { default as EnvironmentManagementGuidePage } from './environment-management/EnvironmentManagementGuidePage';
+
+// Export Framework Status pages
+export { default as FrameworkStatusPage } from './framework-status/FrameworkStatusPage';
+export { default as FrameworkStatusGuidePage } from './framework-status/FrameworkStatusGuidePage';
+
+// Export Getting Started pages
+export { default as GettingStartedPage } from './getting-started/GettingStartedPage';
+export { default as GettingStartedGuidePage } from './getting-started/GettingStartedGuidePage';
+
+// Export Optional Dependencies pages
+export { default as OptionalDependenciesPage } from './optional-dependencies/OptionalDependenciesPage';
+
+// Export Security Framework pages
+export { default as SecurityFrameworkPage } from './security-framework/SecurityFrameworkPage';
+export { default as SecurityFrameworkGuidePage } from './security-framework/SecurityFrameworkGuidePage';
+export { default as DevilSecurityPage } from './security-framework/DevilSecurityPage';
+
+// Export Token Usage Control pages
+export { default as TokenUsageControlPage } from './token-usage-control/TokenUsageControlPage';
+export { default as TokenUsageControlGuidePage } from './token-usage-control/TokenUsageControlGuidePage';
+
+// Export WebSocket Framework pages
+export { default as WebSocketFrameworkPage } from './websocket-framework/WebSocketFrameworkPage';
+export { default as WebSocketFrameworkGuidePage } from './websocket-framework/WebSocketFrameworkGuidePage';
+export { default as BrolostackWSClientPage } from './websocket-framework/BrolostackWSClientPage';
+export { default as BrolostackWSMultiAgentPage } from './websocket-framework/BrolostackWSMultiAgentPage';
